35d6fc9376def05f9bb2ecfaaea66f6033c54e28
Philipp Gampe <tastycrysis@gmail.com>
2024-12-04 16:25:00 +0100

    challenge done

--- js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  console.log("Dark");
+});
+
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+  console.log("Light");
+});
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+  console.log("toggle");
+});
