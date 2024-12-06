bb93514ab1c3e1d4760e7aea9efd995a77c26652
Philipp Gampe <tastycrysis@gmail.com>
2024-12-06 12:31:57 +0100

    solve challenge objects and arrays

--- js-objects-and-arrays/js-objects-and-arrays_objects-and-arrays/js/index.js
+++ js-objects-and-arrays/js-objects-and-arrays_objects-and-arrays/js/index.js
@@ -4,9 +4,9 @@ const people = [
   {
     name: "Homer",
     age: 39,
-    parents: { mothersName: "Mona", fathersName: "Abe" },
-    pet: { animal: "dog", name: "Santa's Little Helper" },
-    friend: { name: "Moe Szyslak", work: "owner of Moes Tavern" },
+    parents: {mothersName: "Mona", fathersName: "Abe"},
+    pet: {animal: "dog", name: "Santa's Little Helper"},
+    friend: {name: "Moe Szyslak", work: "owner of Moes Tavern"},
     "other friends": ["Barney Gumble", "Carl Carlson", "Lenny Leonard"],
     color_cloth: "white",
   },
@@ -17,27 +17,27 @@ const people = [
       mothersName: "Jacqueline Bouvier",
       fathersName: "Clancy Bouvier",
     },
-    pet: { animal: "dog", name: "Santa's Little Helper" },
+    pet: {animal: "dog", name: "Santa's Little Helper"},
     kids: [
-      { name: "Bart", child_number: "first" },
-      { name: "Lisa", child_number: "second" },
-      { name: "Maggie", child_number: "third" },
+      {name: "Bart", child_number: "first"},
+      {name: "Lisa", child_number: "second"},
+      {name: "Maggie", child_number: "third"},
     ],
     color_cloth: "lightgreen",
   },
   {
     name: "Bart",
     age: 10,
-    parents: { mothersName: "Marge", fathersName: "Homer" },
-    pet: { animal: "dog", name: "Santa's Little Helper" },
+    parents: {mothersName: "Marge", fathersName: "Homer"},
+    pet: {animal: "dog", name: "Santa's Little Helper"},
     quotes: ["Ay Caramba!", "Eat my shorts!", "I didnt do it!"],
     color_cloth: "red",
   },
   {
     name: "Lisa",
     age: 8,
-    parents: { mothersName: "Marge", fathersName: "Homer" },
-    pet: { animal: "dog", name: "Santa's Little Helper" },
+    parents: {mothersName: "Marge", fathersName: "Homer"},
+    pet: {animal: "dog", name: "Santa's Little Helper"},
     quotes: [
       "Bart!",
       "If anyone wants me, Ill be in my room.",
@@ -50,27 +50,27 @@ const people = [
 // EXERCISE 1
 // Set the variable `nameOfFirstPerson` to the name of the first person in the array.
 
-const nameOfFirstPerson = "change me";
+const nameOfFirstPerson = people[0].name;
 
 // EXERCISE 2
 // Set the variable `homersOtherFriends` to the other friends of Homer.
 
-const homersOtherFriends = "change me";
+const homersOtherFriends = people[0]["other friends"];
 
 // EXERCISE 3
 // Set the variable `ageOfThirdPerson` to the age of the third person.
 
-const ageOfThirdPerson = "change me";
+const ageOfThirdPerson = people[2].age;
 
 // EXERCISE 4
 // Set the variable `mothersNameOfSecondPerson` to the mother's name of the second person.
 
-const mothersNameOfSecondPerson = "change me";
+const mothersNameOfSecondPerson = people[1].parents["mothersName"];
 
 // EXERCISE 5
 // Set the variable `animalTypeOfLisasPet` to the type of animal that Lisa has as a pet.
 
-const animalTypeOfLisasPet = "change me";
+const animalTypeOfLisasPet = people[3].pet["animal"];
 
 export {
   nameOfFirstPerson,
