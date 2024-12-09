1e389320ca4dff6c836a16e0baee1c3520547f23
Philipp Gampe <tastycrysis@gmail.com>
2024-12-09 15:31:39 +0100

    solve js-switch-values challenge

--- js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -1,13 +1,29 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  firstInput.value =
+    firstInput.value === firstInput.value.toUpperCase()
+      ? firstInput.value.toLowerCase()
+      : firstInput.value.toUpperCase();
+
+  secondInput.value =
+    secondInput.value === secondInput.value.toUpperCase()
+      ? secondInput.value.toLowerCase()
+      : secondInput.value.toUpperCase();
 });
