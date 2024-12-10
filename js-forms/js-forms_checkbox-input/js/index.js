72e212241f21f875c9aeabb0e436154da925ce5d
Philipp Gampe <tastycrysis@gmail.com>
2024-12-10 13:14:21 +0100

    solve js-checkbox-input challenge

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,40 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccess() {
+  successMessage.setAttribute("hidden", "");
+}
+
+function showSuccess() {
+  successMessage.removeAttribute("hidden");
+}
+
+hideTosError();
+hideSuccess();
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
 
+  if (!tosCheckbox.checked) {
+    showTosError();
+    return;
+  }
+
+  showSuccess();
+
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  /* alert("Form submitted"); */
+  event.target.reset();
 });
