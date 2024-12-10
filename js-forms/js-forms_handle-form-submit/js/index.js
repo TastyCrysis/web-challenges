0d8b0270ef9d293a81de397da1d41a519202c83e
Philipp Gampe <tastycrysis@gmail.com>
2024-12-10 11:57:04 +0100

    solve handle-form-submit challenge

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,29 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.target.elements;
+  console.log(formElements);
+
+  const formData = {
+    firstname: form.firstName.value,
+    lastname: form.lastName.value,
+    age: form.age.value,
+    email: form.email.value,
+    yourComplaint: form.complaint.value,
+    details: form.details.value,
+    howBad: form.badness.value,
+    orderdate: form.orderDate.value,
+  };
+  console.log(formData);
+
+  const ageBadnessSum = event.target.badness.value + event.target.age.value;
+  console.log(
+    "The age-badness-sum of " +
+      event.target.firstName.value +
+      " is " +
+      ageBadnessSum
+  );
+});
