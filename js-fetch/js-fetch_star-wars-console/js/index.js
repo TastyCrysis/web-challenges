34fdab95a5e75a29ad3ad1debb4d79468075a10b
Philipp Gampe <tastycrysis@gmail.com>
2024-12-16 16:29:27 +0100

    solve js-star-wars-console challenge

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,10 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
-
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log("eye color of R2-D2:", data.results[2].eye_color);
+}
 fetchData();
