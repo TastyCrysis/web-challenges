63013ee7a5a3f6824f755378dc69e7049ee2efe7
Philipp Gampe <tastycrysis@gmail.com>
2025-01-06 11:28:10 +0100

    solve hello-world-article challenge

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello world!</h1>
+      <p>Hello React...</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
