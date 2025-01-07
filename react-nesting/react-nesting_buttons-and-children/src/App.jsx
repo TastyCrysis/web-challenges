d2808c5a29616dc6d9ff9f35f55d00ff233df769
Philipp Gampe <tastycrysis@gmail.com>
2025-01-07 11:20:24 +0100

    solve react-buttons-and-children challenge

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click me!</Button>
+      <Button>Click me too!</Button>
+      <Button>Click me twice!</Button>
+      <Button>Click me three times!</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
