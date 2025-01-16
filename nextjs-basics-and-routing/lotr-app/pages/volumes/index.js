a007f88f97d8eafa46ed355702ba08d84519759d
Philipp Gampe <tastycrysis@gmail.com>
2025-01-16 14:03:24 +0100

    fix link mapping

--- nextjs-basics-and-routing/lotr-app/pages/volumes/index.js
+++ nextjs-basics-and-routing/lotr-app/pages/volumes/index.js
@@ -6,36 +6,12 @@ export default function Volumes() {
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
-      <h2>All Volumes</h2>
       <ul>
-        <li>
-          <Link
-            href={`/volumes/${
-              volumes.find(({ slug }) => slug === "the-fellowship-of-the-ring")
-                .slug
-            }`}
-          >
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`/volumes/${
-              volumes.find(({ slug }) => slug === "the-two-towers").slug
-            }`}
-          >
-            The Two Towers
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`/volumes/${
-              volumes.find(({ slug }) => slug === "the-return-of-the-king").slug
-            }`}
-          >
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
