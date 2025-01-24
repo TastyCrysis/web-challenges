e58f88207c980f69b74c66ae86ed5de125b1aad3
Philipp Gampe <tastycrysis@gmail.com>
2025-01-24 11:34:13 +0100

    fix createServer request

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -4,11 +4,16 @@ import Chance from "chance";
 const chance = new Chance();
 
 export const server = createServer((request, response) => {
-  const name = chance.name();
-  const age = chance.age();
-  const profession = chance.profession({ rank: true });
-  response.statusCode = 200;
-  response.end(
-    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
-  );
+  if (request.url === "/") {
+    const name = chance.name();
+    const age = chance.age();
+    const profession = chance.profession({ rank: true });
+    response.statusCode = 200;
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
 });
