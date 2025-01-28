225bb6333975371eb539900f288788250145da59
Philipp Gampe <tastycrysis@gmail.com>
2025-01-28 16:14:00 +0100

    solve backend-update-and-deletete-products challenge

--- backend-update-and-delete/backend-update-and-delete_products/pages/index.js
+++ backend-update-and-delete/backend-update-and-delete_products/pages/index.js
@@ -1,8 +1,28 @@
 import styled from "styled-components";
 import ProductList from "@/components/ProductList";
 import ProductForm from "@/components/ProductForm";
+import useSWR from "swr";
 
 export default function HomePage() {
+  const { mutate } = useSWR("/api/products");
+
+  async function handleAddProduct(productData) {
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+  }
+
   return (
     <>
       <Heading>
@@ -11,7 +31,7 @@ export default function HomePage() {
         </span>
         Fish Shop
       </Heading>
-      <ProductForm />
+      <ProductForm onSubmit={handleAddProduct} />
       <hr />
       <ProductList />
     </>
